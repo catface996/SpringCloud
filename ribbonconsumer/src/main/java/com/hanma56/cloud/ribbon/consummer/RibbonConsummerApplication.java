4f1d8b485205b8f9074f30221bd65a3a63810e89
zhangzhong <zlyxzq@163.com>
2018-02-03 09:44:49 +0800

    1.增加ribbon的熔断保护

--- ribbonconsumer/src/main/java/com/hanma56/cloud/ribbon/consummer/RibbonConsummerApplication.java
+++ ribbonconsumer/src/main/java/com/hanma56/cloud/ribbon/consummer/RibbonConsummerApplication.java
@@ -2,10 +2,12 @@ package com.hanma56.cloud.ribbon.consummer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class RibbonConsummerApplication {
 
     public static void main(String[] args) {
--- ribbonconsumer/src/main/java/com/hanma56/cloud/ribbon/consummer/service/HelloService.java
+++ ribbonconsumer/src/main/java/com/hanma56/cloud/ribbon/consummer/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.hanma56.cloud.ribbon.consummer.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -18,7 +19,17 @@ public class HelloService {
      * 调用 master data 的 hello 服务
      * @return
      */
+    @HystrixCommand(fallbackMethod = "findByIdFallback")
     public String hiService() {
         return restTemplate.getForObject("http://master-data/hello",String.class);
+    }
+
+    /**
+     * 当 springms-provider-user 服务宕机或者不可用时，即请求超时后会调用此方法。
+     *
+     * @return
+     */
+    public String findByIdFallback() {
+        return "断路";
     }
 }
